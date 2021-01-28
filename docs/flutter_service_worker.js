b9b0a18fe4278dc422cf8796d7c34e34c7ae709f
UsamaSarwar <UsamaSarwarOfficial@gmail.com>
2021-01-29 02:25:40 +0500

    v1.0.4

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,10 +25,10 @@ const RESOURCES = {
 "icons/Icon-512.png": "04033cc7fe11bb4699744edd30603df8",
 "index.html": "c54c5dd366b8bbe6c8e03ed838cee7ba",
 "/": "c54c5dd366b8bbe6c8e03ed838cee7ba",
-"main.dart.js": "d39ed8b9c734ed39c3246934049984c0",
+"main.dart.js": "0941e7db2b464e7f7772f690da74ad85",
 "manifest.json": "dc2a60a4d6c24a1bd2eaf3d5fe6e25d8",
 "script.js": "36ca37d7e47e12dc37e4f63bd6a4711d",
-"version.json": "5760cef78cce11e3731876b80511f9e5"
+"version.json": "8966a3ce14271ce70a09185ef26e810d"
 };
 
 // The application shell files that are downloaded before a service worker can
